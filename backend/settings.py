bb68036f1fdcc673364cdfcf2836cc7d9529ffc9
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2017-01-21 15:30:50 -0500

    retrying Clarifai

--- backend/settings.py
+++ backend/settings.py
@@ -2,7 +2,7 @@ from os import path
 
 BASE_DIR = path.dirname(path.abspath(__file__))
 
-API_KEY_FILE = path.join(path.dirname(BASE_DIR), "watson_api.json")
+API_KEY_FILE = path.join(path.dirname(BASE_DIR), "microsoft_api.json")
 
 DB_PATH = path.join(path.dirname(BASE_DIR), "db")
 
--- backend/train.py
+++ backend/train.py
@@ -7,13 +7,15 @@ import cognitive_face as CF
 from settings import API_KEY_FILE, TRAIN_PATH
 
 TRAIN_DATA_DIRS = glob(TRAIN_PATH + "/*.zip")[0]
-print TRAIN_DATA_DIRS
+
 
 with open(API_KEY_FILE) as api_key:
 
-    KEY = json.load(api_key)["api_key"]
+    KEY = json.load(api_key)["key1"]
     CF.Key.set(KEY)
 
     img_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
-    result = CF.face.detect(img_url)
-    print result
+    # result = CF.face_list.get("")
+    CF.face_list.add_face(img_url, "id01")
+
+    print(CF.face_list.get("id01"))
