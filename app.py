3aeeb280090bae71493927457272f0683759b96e
Jaime5 <Jaime5@UMBC.edu>
2017-01-21 17:18:34 -0500

    add generic user portal

--- app.py
+++ app.py
@@ -1,136 +1,119 @@
 import os
 
-from flask import Flask, jsonify, request
-from flask_mail import Mail
-from flask_sqlalchemy import SQLAlchemy
-from flask_user import UserManager, UserMixin, SQLAlchemyAdapter
+from flask import Flask, abort, request, jsonify, g, url_for
+from flask.ext.httpauth import HTTPBasicAuth
+from flask.ext.sqlalchemy import SQLAlchemy
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
+from passlib.apps import custom_app_context as pwd_context
 
 from backend.capital_one import get_recent_deposits, get_customer_information
 
+""" Flask application factory """
+
+# Setup Flask app and app.config
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
-# Use a Class-based config to avoid needing a 2nd file
-# os.getenv() enables configuration through OS environment variables
-class ConfigClass(object):
-	# Flask settings
-	SECRET_KEY = os.getenv('SECRET_KEY', 'THIS IS AN INSECURE SECRET')
-
-	SQLALCHEMY_DATABASE_URI = os.getenv(
-		'DATABASE_URL',
-		'sqlite:///basic_app.sqlite'
-	)
-	CSRF_ENABLED = True
-
-	USER_ENABLE_CONFIRM_EMAIL = False
-	USER_ENABLE_FORGOT_PASSWORD = True  # Allow users to reset their passwords
-	# Requires USER_ENABLE_EMAIL=True
-
-	USER_ENABLE_LOGIN_WITHOUT_CONFIRM_EMAIL = True
-	# Allow users to login without a
-	# confirmed email address
-	# Protect views using @confirm_email_required
-
-	USER_ENABLE_EMAIL = False  # Register with Email
-	# Requires USER_ENABLE_REGISTRATION=True
+# extensions
+db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
 
 
-	# Flask-Mail settings
-	MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'email@example.com')
-	MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'password')
-	MAIL_DEFAULT_SENDER = os.getenv(
-		'MAIL_DEFAULT_SENDER', '"MyApp" <noreply@example.com>'
-	)
-	MAIL_SERVER = os.getenv(
-		'MAIL_SERVER', 'smtp.gmail.com'
-	)
-	MAIL_PORT = int(os.getenv('MAIL_PORT', '465'))
-	MAIL_USE_SSL = int(os.getenv('MAIL_USE_SSL', True))
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(32), index=True)
+	password_hash = db.Column(db.String(64))
 
-	# Flask-User settings
-	USER_APP_NAME = "AppName"  # Used by email templates
+	def hash_password(self, password):
+		self.password_hash = pwd_context.encrypt(password)
+
+	def verify_password(self, password):
+		return pwd_context.verify(password, self.password_hash)
+
+	def generate_auth_token(self, expiration=600):
+		s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+		return s.dumps({'id': self.id})
+
+	@staticmethod
+	def verify_auth_token(token):
+		s = Serializer(app.config['SECRET_KEY'])
+		try:
+			data = s.loads(token)
+		except SignatureExpired:
+			return None  # valid token, but expired
+		except BadSignature:
+			return None  # invalid token
+		user = User.query.get(data['id'])
+		return user
 
 
-def create_app():
-	""" Flask application factory """
+@auth.verify_password
+def verify_password(username_or_token, password):
+	# first try to authenticate by token
+	user = User.verify_auth_token(username_or_token)
+	if not user:
+		# try to authenticate with username/password
+		user = User.query.filter_by(username=username_or_token).first()
+		if not user or not user.verify_password(password):
+			return False
+	g.user = user
+	return True
 
-	# Setup Flask app and app.config
-	app = Flask(__name__)
-	app.config.from_object(__name__ + '.ConfigClass')
 
-	# Initialize Flask extensions
-	db = SQLAlchemy(app)  # Initialize Flask-SQLAlchemy
-	mail = Mail(app)  # Initialize Flask-Mail
+@app.route('/api/users', methods=['POST'])
+def new_user():
+	username = request.json.get('username')
+	password = request.json.get('password')
+	if username is None or password is None:
+		abort(400)  # missing arguments
+	if User.query.filter_by(username=username).first() is not None:
+		abort(400)  # existing user
+	user = User(username=username)
+	user.hash_password(password)
+	db.session.add(user)
+	db.session.commit()
+	return (jsonify({'username': user.username}), 201,
+	        {'Location': url_for('get_user', id=user.id, _external=True)})
 
-	# Define the User data model. Make sure to add flask_user UserMixin !!!
-	class User(db.Model, UserMixin):
-		id = db.Column(db.Integer, primary_key=True)
 
-		# User authentication information
-		username = db.Column(db.String(50), nullable=False, unique=True)
-		password = db.Column(db.String(255), nullable=False, server_default='')
+@app.route('/api/users/<int:id>')
+def get_user(id):
+	user = User.query.get(id)
+	if not user:
+		abort(400)
+	return jsonify({'username': user.username})
 
-		# User email information
-		# email = db.Column(db.String(255), nullable=False, unique=True)
-		confirmed_at = db.Column(db.DateTime())
 
-		# User information
-		active = db.Column(
-			'is_active', db.Boolean(),
-			nullable=False, server_default='0'
-		)
-		first_name = db.Column(
-			db.String(100), nullable=False,
-			server_default=''
-		)
-		last_name = db.Column(
-			db.String(100), nullable=False, server_default=''
-		)
+@app.route('/api/token')
+@auth.login_required
+def get_auth_token():
+	token = g.user.generate_auth_token(600)
+	return jsonify({'token': token.decode('ascii'), 'duration': 600})
 
-	# Create all database tables
-	db.create_all()
 
-	# Setup Flask-User
-	db_adapter = SQLAlchemyAdapter(db, User)  # Register the User model
-	user_manager = UserManager(db_adapter, app)  # Initialize Flask-User
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+	return jsonify({'data': 'Hello, %s!' % g.user.username})
 
-	@app.route("/capital_one/recent_deposits")
-	def recent_deposits():
-		return jsonify(results=get_recent_deposits())
 
-	@app.route("/capital_one/customer/<customer_id>")
-	def customer(customer_id):
-		return jsonify(results=get_customer_information(customer_id))
+@app.route("/capital_one/recent_deposits")
+def recent_deposits():
+	return jsonify(results=get_recent_deposits())
 
-	@app.route("/register", methods=["POST"])
-	def register():
-		username = request.args.get("username")
-		email = request.args.get("email")
-		password = request.args.get("password")
 
-		print(username, email, password)
-
-		"""
-		Username
-		Email
-		Password
-
-		:return:
-		"""
-		return jsonify(success=True)
-
-	@app.route("/api/login/")
-	def login():
-		pass
-
-	@app.route("/api/logout")
-	def logout():
-		pass
-
-	return app
+@app.route("/capital_one/customer/<customer_id>")
+def customer(customer_id):
+	return jsonify(results=get_customer_information(customer_id))
 
 
 if __name__ == "__main__":
-	app = create_app()
 
+	if not os.path.exists('db.sqlite'):
+		db.create_all()
 	app.run(debug=True)
