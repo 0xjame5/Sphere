52f7d2ea945acd07a47dfe6bf6a228fd4b1c5556
Jaime5 <Jaime5@UMBC.edu>
2017-01-22 02:16:50 -0500

    prep for deployment 2

--- manage.py
+++ manage.py
@@ -1,22 +1,13 @@
 from flask_script import Manager
 
 from app import app
-from backend.capital_one_api import get_list_of_checking_accounts, delete_all_non_checking_accounts
 
 manager = Manager(app)
 
 
 @manager.command
 def generate_sql_db():
-	accounts = get_list_of_checking_accounts()
-
-	print(accounts)
-	# GENERATE_DB HERE
-
-
-@manager.command
-def delete_non_checking_accounts():
-	delete_all_non_checking_accounts()
+	pass
 
 
 if __name__ == '__main__':
